fbfe3f9ae81ebaabd26a29c13b8069a172d6ff04
Sandip Bhetariya <sandip.bh.java@gmail.com>
2020-07-17 11:10:57 +0530

    commit

--- course-microservice-server-master/src/main/resources/db/migration/V1__1.DDL.sql
+++ course-microservice-server-master/src/main/resources/db/migration/V1__1.DDL.sql
@@ -7,3 +7,17 @@ CREATE TABLE `courseinfo` (
   `price_international` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
+
+
+
+
+
+
+
+
+INSERT INTO `courseinfo` (`id`, `course_country_id`, `institute_name`, `name`, `price_domestic`, `price_international`) VALUES (1,1,'oxford','Spring Boot',175,200);
+INSERT INTO `courseinfo` (`id`, `course_country_id`, `institute_name`, `name`, `price_domestic`, `price_international`) VALUES (2,2,'cambridge','J2ee',225,275);
+INSERT INTO `courseinfo` (`id`, `course_country_id`, `institute_name`, `name`, `price_domestic`, `price_international`) VALUES (3,2,'pennsylvania','PHP',125,145);
+
+
--- user-microservice-server-master/src/main/resources/db/migration/V1__1.DDL.sql
+++ user-microservice-server-master/src/main/resources/db/migration/V1__1.DDL.sql
@@ -11,3 +11,15 @@ CREATE TABLE `userinfo` (
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
+
+
+
+INSERT INTO `countryinfo` (`id`, `name`) VALUES ('1', 'India');
+INSERT INTO `countryinfo` (`id`, `name`) VALUES ('2', 'US');
+INSERT INTO `countryinfo` (`id`, `name`) VALUES ('3', 'UK');
+
+
+INSERT INTO `userinfo` (`id`, `countryid`, `email`, `name`) VALUES ('1', '1', 'test1@gmail.com', 'test1');
+INSERT INTO `userinfo` (`id`, `countryid`, `email`, `name`) VALUES ('2', '2', 'test2@gmail.com', 'test2');
+INSERT INTO `userinfo` (`id`, `countryid`, `email`, `name`) VALUES ('3', '3', 'test3@gmail.com', 'test3');
